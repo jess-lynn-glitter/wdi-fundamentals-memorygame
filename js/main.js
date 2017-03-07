682d08ee829fc04cefa1ee469cd43c7837ed7103
Jessica Wood <jesswood@Jessicas-MacBook-Pro.local>
2017-03-07 14:02:55 -0700

    created game logic

---
+++ js/main.js
@@ -0,0 +1,39 @@
+var cards = ["queen","queen","king","king"];
+
+var cardsInPlay = [];
+
+
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
+};
+
+var flipCard = function(cardId) {
+
+	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	console.log("User flipped " + cards[cardId]);
+
+		//if (cardsInPlay.length === 2);	
+	};
+
+flipCard(0);
+flipCard(1);
+
+
+	
+
+	
+
+
+
+
+
+
+
+
+
