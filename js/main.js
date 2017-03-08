6244e4aaba50b055f4c89114fb2cca6211ecec15
Jessica Wood <jesswood@Jessicas-MacBook-Pro.local>
2017-03-07 23:31:58 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,28 +1,50 @@
-var cards = ["queen","queen","king","king"];
+var cards = [
+	{
+		rank: 'queen',
+		suit: 'hearts',
+		cardImage: 'images/queen-of-hearts.png'
+	},
+	{
+		rank: 'queen',
+		suit: 'diamonds',
+		cardImage: 'images/queen-of-diamonds.png'
+	},
+	{
+		rank: 'king',
+		suit: 'hearts',
+		cardImage: 'images/king-of-hearts.png'
+	},
+	{
+		rank: 'king',
+		suit: 'diamonds',
+		cardImage: 'images/king-of-diamonds.png'
+	}
+];
 
 var cardsInPlay = [];
 
 
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+		alert("You found a match!");
 	} else {
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 	}
 };
 
 var flipCard = function(cardId) {
+		if (cardsInPlay.length === 2);	{
+		checkForMatch();
+		console.log("User flipped " + cards[cardId].rank);
+		cardsInPlay.push(cards[cardId].rank);
 
-	cardsInPlay.push(cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
-
-	console.log("User flipped " + cards[cardId]);
-
-		//if (cardsInPlay.length === 2);	
-	};
+		console.log(cards[cardId].cardImage);
+		console.log(cards[cardId].suit);		
+	}	
+};
 
 flipCard(0);
-flipCard(1);
+flipCard(2);
 
 
 	
---
+++ main.js
@@ -0,0 +1,37 @@
+var cards = ["queen","queen","king","king"];
+
+var cardsInPlay = [];
+
+
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
+};
+
+var flipCard = function(cardId) {
+	if (cardsInPlay.length === 2);	{
+		checkForMatch();
+		console.log("User flipped " + cards[cardId]);
+		cardsInPlay.push(cards[cardId]);
+	}	
+};
+
+flipCard(0);
+flipCard(1);
+
+	
+	
+
+	
+
+
+
+
+
+
+
+
+
